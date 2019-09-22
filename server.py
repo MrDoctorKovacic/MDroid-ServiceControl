2338d4ae5f69b1ae65b8bb534ed1fd2400dd8c37
MrDoctorKovacic <qcasey1@gmail.com>
2019-09-22 17:51:52 +0000

    fix indents again

--- server.py
+++ server.py
@@ -5,17 +5,17 @@ PORT = 5350
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-    	if "reboot" in self.path:
-	    self.send_response(200)
-	    self.end_headers()
-	    os.system("reboot now") 
-	elif "shutdown" in self.path:
-	    self.send_response(200)
-	    self.end_headers()
-	    os.system("shutdown now") 
-	else:
-	    self.send_response(200)
-	    self.end_headers()
+        if "reboot" in self.path:
+            self.send_response(200)
+            self.end_headers()
+            os.system("reboot now") 
+        elif "shutdown" in self.path:
+            self.send_response(200)
+            self.end_headers()
+            os.system("shutdown now") 
+        else:
+            self.send_response(200)
+            self.end_headers()
 
 httpd = HTTPServer(("", PORT), SimpleHTTPRequestHandler)
 httpd.serve_forever()
